9e0ede7f064eab00f8da63a346ddd27daa411ad1
Dillon Christensen <j.dillon.christensen@gmail.com>
2020-02-10 11:08:34 -0600

    working prototype

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const URLS = [
+    'https://apps.jw.org/GETPUBMEDIALINKS?output=json&pub=osg&fileformat=MP3&alllangs=0&langwritten=E&txtCMSLang=E',
+]
+
+const getFiles = async (url) => {
+    const res = await fetch(url)
+    if (res.ok) {
+        const data = await res.json()
+        return data.files.E.MP3
+            .map((file) => {
+                return {
+                    title: file.title,
+                    url: file.file.url,
+                    art: file.trackImage.url,
+                    raw: { ...file },
+                }
+            })
+    }
+    return []
+}
+
+class App extends React.Component {
+    state = {
+        songs: [],
+        song: null,
+    }
+
+    componentDidMount = () => {
+        getFiles(URLS[0])
+            .then((songs) => {
+                this.chooseRandomSong(songs)
+            })
+    }
+
+    chooseRandomSong = (songs) => {
+        if (!songs || !Array.isArray(songs)) {
+            songs = this.state.songs
+        }
+        const i = ~~(Math.random() * songs.length)
+        const song = songs[i]
+        this.setState({
+            songs: songs.filter((ignore, si) => si !== i),
+            song,
+        })
+    }
+
+    render = () => {
+        const { song } = this.state
+
+        if (!song) {
+            return (
+                <div className="App">
+                    <h1>Loading...</h1>
+                </div>
+            )
+        }
+
+        return (
+            <div className="App">
+                <h1>{song.title}</h1>
+                <img src={song.art} alt={song.title} />
+                <br />
+                <audio key={song.url} autoPlay controls onEnded={this.chooseRandomSong}>
+                    <source src={song.url} type="audio/mpeg" />
+                </audio>
+            </div>
+        )
+    }
 }
 
 export default App;
