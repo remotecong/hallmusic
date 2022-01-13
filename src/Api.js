097876a50237ad41b917b9c010c52e49d318a7a0
Dillon Christensen <j.dillon.christensen@gmail.com>
2022-01-13 12:42:56 -0600

    added original songs to playlist

--- src/Api.js
+++ src/Api.js
@@ -1,29 +1,27 @@
 export const URLS = [
-    //'https://apps.jw.org/GETPUBMEDIALINKS?output=json&pub=osg&fileformat=MP3&alllangs=0&langwritten=E&txtCMSLang=E', //original songs
-    'https://pubmedia.jw-api.org/GETPUBMEDIALINKS?output=json&pub=sjjm&fileformat=MP3&alllangs=0&langwritten=E&txtCMSLang=E', //meetings
-    'https://pubmedia.jw-api.org/GETPUBMEDIALINKS?output=json&pub=sjjc&fileformat=MP3&alllangs=0&langwritten=E&txtCMSLang=E', //chorus
-    'https://pubmedia.jw-api.org/GETPUBMEDIALINKS?output=json&pub=sjji&fileformat=MP3&alllangs=0&langwritten=E&txtCMSLang=E', //instrumentals
-]
+  "https://apps.jw.org/GETPUBMEDIALINKS?output=json&pub=osg&fileformat=MP3&alllangs=0&langwritten=E&txtCMSLang=E", //original songs
+  "https://pubmedia.jw-api.org/GETPUBMEDIALINKS?output=json&pub=sjjm&fileformat=MP3&alllangs=0&langwritten=E&txtCMSLang=E", //meetings
+  "https://pubmedia.jw-api.org/GETPUBMEDIALINKS?output=json&pub=sjjc&fileformat=MP3&alllangs=0&langwritten=E&txtCMSLang=E", //chorus
+  "https://pubmedia.jw-api.org/GETPUBMEDIALINKS?output=json&pub=sjji&fileformat=MP3&alllangs=0&langwritten=E&txtCMSLang=E" //instrumentals
+];
 
-export const getFiles = async (url) => {
-    const res = await fetch(url)
-    if (res.ok) {
-        const data = await res.json()
-        return data.files.E.MP3
-            .map((file) => {
-                return {
-                    title: file.title,
-                    url: file.file.url,
-                    art: file.trackImage.url,
-                }
-            })
-    }
-    return []
-}
+export const getFiles = async url => {
+  const res = await fetch(url);
+  if (res.ok) {
+    const data = await res.json();
+    return data.files.E.MP3.map(file => {
+      return {
+        title: file.title,
+        url: file.file.url,
+        art: file.trackImage.url
+      };
+    });
+  }
+  return [];
+};
 
 export const loadAllSongs = async () => {
-    return await Promise.all(URLS.map(getFiles))
-        .then((songs) => {
-            return songs.reduce((coll, list) => coll.concat(list), []);
-        });
-}
+  return await Promise.all(URLS.map(getFiles)).then(songs => {
+    return songs.reduce((coll, list) => coll.concat(list), []);
+  });
+};
